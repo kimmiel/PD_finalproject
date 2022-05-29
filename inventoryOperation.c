a3a41b984711500b953ab22546d8aec951a33669
gahwa17 <s24527109@gmail.com>
2022-05-29 21:42:14 +0800

    完成基本結構設計(Inventory, Order)

--- inventoryOperation.c
+++ inventoryOperation.c
@@ -1,4 +1,4 @@
-#include "basic.h"
+#include "myDS.h"
 #include "inventoryOperation.h"
 #include "sort.h"
 
---
+++ myDS.h
@@ -0,0 +1,35 @@
+#include "basic.h"
+
+#ifndef myDS_H
+#define myDS_H
+
+enum bookType {EDU , FIN, NOVEL};
+
+struct inventory {
+    int inventoryId;
+    char inventoryName [MAX_BOOK_NAME];
+    double price;
+    int quantity;
+    struct inventory *next;
+};
+
+struct Categoty {
+    enum bookType type;
+    struct Inventory *Inventory_head;
+};
+
+/****************************************
+ * Order: use queue to implement *
+ ****************************************/
+struct Order {
+    char CustomerName [MAX_CUSTOMER_NAME];
+    int orderId;
+    int inventoryIds [20];
+    int inventoryQuantity [20];
+    int totalPrice;
+    time_t orderDate;
+    struct Order *prev;
+    struct Order *next;
+};
+
+#endif
